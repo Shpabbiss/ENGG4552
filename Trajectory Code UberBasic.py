4f1b571f0d0930e0e05d5ff1eabf9694b954f655
Shpabbiss <69231620+Shpabbiss@users.noreply.github.com>
2021-04-08 16:08:30 +1000

    implemented scale height formula
    Not 100% sure on the formula as there is conflicting information - needs revisiting. Also need to determine what the absolute temp should be.

--- Trajectory Code UberBasic.py
+++ Trajectory Code UberBasic.py
@@ -1,1 +1,15 @@
 """Initial Trajectory Bullshit"""
+
+init_altitude = 400*10**3 #[m]
+init_velocity = 8000    #[m/s]
+surface_density = 1.225 #[kg/m^3]
+gas_constant = 287.05   #[J/kg K]
+abs_temp = 0 + 273.15   #[K] - Not 100% sure what this should be??
+g = 9.81
+
+
+def scaleheight():   #Need to check this formula - conflicting information
+    b = - (g/gas_constant)/(abs_temp)
+    return(b)
+
+    
