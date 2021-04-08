66e421808cc46e8af42f11edeecee7b550817bc7
Shpabbiss <69231620+Shpabbiss@users.noreply.github.com>
2021-04-08 17:23:27 +1000

    velocity vs altitude plot working
    note that the plot is innaccurate at it approaches the ground - assumptions fall apart here.

--- Trajectory Code UberBasic.py
+++ Trajectory Code UberBasic.py
@@ -1,37 +1,84 @@
 """Initial Trajectory Bullshit"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+"""Initial Conditions"""
 init_altitude = 400*10**3 #[m]
 init_velocity = 8000    #[m/s]
+gamma = np.radians(12)               #[degrees]
+
+
+"""Constants"""
 surface_density = 1.225 #[kg/m^3]
 gas_constant = 287.05   #[J/kg K]
 abs_temp = 0 + 273.15   #[K] - Not 100% sure what this should be??
 g = 9.81
+y0 = 6620           #[m] Scale height
 beta = 1/6620       #Beta^-1 - retrieved from AERO4800 textbook
-beta_op2 = 0.000139 #Beta^-1 - retrieved from Lit Review (Portfolio)
+#beta_op2 = 0.000139 #Beta^-1 - retrieved from Lit Review (Portfolio)
+
+
+"""US Atmospheric Standards"""
 US_atmo_vals = [1.225,0.9093,0.7364,0.59,0.4135,0.1948,0.04008,0.001027,\
                 0.00001846]  #US Standard Atmospheric Densities
 US_atmo_heights = [0,3,5,7,10,15,25,50,80] #Corresponding altitudes
 
 
-def scaleheight():   #Need to check this formula - conflicting information
-    b = - (g/gas_constant)/(abs_temp)
-    return(b)
+"""Vehicle Properties"""
+mass = 10      #[kg]
+Cd = 1          #Coefficient of Drag
+Cl = 0          #Coefficient of Lift
+S = 2.5           #[m^2] Reference Area - Based on CAD model
+BC = mass/(S*Cd)
+
+"""Functions/Calculations"""
+
+#def scaleheight():   #Need to check this formula - conflicting information
+ #   b = - (g/gas_constant)/(abs_temp)
+  #  return(b)
 
 def atmo_density(h):  #Calculates density based on altitude
-    rho = surface_density*np.exp(scaleheight()*h)
+    rho = surface_density*np.exp(beta*h)
     return rho
 
 def atmo_density_plot():#Plots density vs altitude + compares with US Standard
-    x_val = np.linspace(0,100*10**3,1000)
-    y_val = []
+    x_vals = np.linspace(0,100*10**3,1000)
+    y_vals = []
     for x in x_val:
         y_val.append(atmo_density(x))
-    plt.plot((x_val/1000),y_val)
-    plt.plot(US_atmo_heights,US_atmo_vals)
+    plt.plot((x_vals/1000),y_vals,label = "Calculated")
+    plt.plot(US_atmo_heights,US_atmo_vals,label = "US Standard")
     plt.title("Atmospheric Density vs Altitude")
+    plt.legend()
     plt.xlabel("Altitude (km)")
     plt.ylabel("Density (kg/m^3)")
+    plt.show()
+
+def velocity(h): #calculates the velocity at a given altitude
+    v = init_velocity*np.exp(-(((surface_density*y0)/(2*BC*np.sin(gamma)))*\
+                             np.exp(-h/y0)))
+    return v
+
+def velocity_plot():
+    x_vals = np.linspace(0,init_altitude,100000)
+    y_vals = []
+    for x in x_vals:
+        y_vals.append(((velocity(x)/init_velocity)*100))
+    plt.plot((x_vals/1000),y_vals)
+    plt.title("Spacecraft Velocity vs Altitude")
+    plt.xlabel("Altitude (km)")
+    plt.ylabel("Velocity (m/s)")
+    plt.show()
+        
+            
+
+
+"""Running the Program"""
+#atmo_density_plot()
+velocity_plot()
+
+
+
     
-atmo_density_plot()
