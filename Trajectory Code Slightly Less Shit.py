68874ede39b01b65b6912463bcaabc2ec808626e
Shpabbiss <69231620+Shpabbiss@users.noreply.github.com>
2021-04-30 15:50:33 +1000

    this do be broken

--- Trajectory Code Slightly Less Shit.py
+++ Trajectory Code Slightly Less Shit.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 """Initial Conditions"""
-init_alt = 400E3 #[m]
+init_alt = 150E3 #[m]
 init_v = 8000    #[m/s]
-init_fpa = 1     #[degrees]
+init_fpa = np.radians(1) #[radians]
 
 """Constants"""
 G = 6.67430E-11  #[Nm^2/kg^2]
@@ -16,15 +16,15 @@ surface_rho = 1.225 #[kg/m^3]
 
 """Vehicle Properties"""
 mass = 10        #[kg]
-noser = 1.0      #[m]
+noser = 0.3      #[m]
 Cd = 1
 Cl = 0
 S = np.pi*noser**2 #[m^2] - Reference Area
 BC = mass/(S*Cd)
 
 """Loop Properties"""
-dt = 0.5 #[s]
-time = 86400 #[s]
+dt = 0.005 #[s]
+time = 500 #[s]
 steps = time/dt + 1
 
 
@@ -52,6 +52,7 @@ def drag(v,rho):
     return Fd
 
 def wackycalcs():
+    t_vals = np.linspace(0,time,int(steps))
     vx = init_v*np.cos(init_fpa)
     vy = init_v*np.sin(init_fpa)
     vx_prev = init_v*np.cos(init_fpa)
@@ -59,21 +60,51 @@ def wackycalcs():
     fpa = init_fpa
     alt = init_alt
     alt_prev = init_alt
-    v_vals = np.array
-    for i,t in enumerate(np.linspace(0,time,steps)):
+    v_vals = np.zeros(int(steps))
+    alt_vals = np.zeros(int(steps))
+    i = 0
+    print(alt)
+    for t in t_vals:
 
+        v = np.sqrt((vx**2)+(vy**2))
+        alt_vals[i] = alt
+        #print("alt = {:.2f}".format(alt))
+        #print("v = {:.2f}".format(v))
+        v_vals[i] = v
         rho = density(alt)
         g = g_acc(alt)
-        Fx = -drag(vx,rho)
-        Fy = mass*g - drag(vy,rho)
-        vx = vx_prev + ((Fx/mass) * t)
-        vy = vy_prev + ((Fy/mass) * t)
-        v = np.sqrt((vx**2)+(vy**2))
+        #print(g)
+        dragval = drag(v,rho)
+        Fx = -dragval*np.cos(fpa)
+        Fy = mass*g - dragval*np.sin(fpa)
+        vx = vx_prev + ((Fx/mass) * dt)
+        vy = vy_prev + ((Fy/mass) * dt)
+        
         fpa = np.arctan(vy/vx)
-        alt = alt_prev - vy*t + 0.5*(Fy/mass)*(t**2)
+        alt = alt_prev - (vy*dt + 0.5*(Fy/mass)*(dt**2))
         vx_prev = vx
         vy_prev = vy
         alt_prev = alt
+        print(np.degrees(fpa))
+        #if t > 445:
+          #  print("rho", rho, "dragval", dragval, Fx)
+        
+        i += 1
+        if alt <= 2000:
+            print("Parachute Deployed at",alt)
+            break 
+        
+        #print(alt)
+        #print(v)
+    plt.plot(t_vals, alt_vals)
+    #plt.xlim(440,460)
+    plt.show()
+    plt.plot(t_vals, v_vals)
+    #plt.xlim(440,460)
+    plt.show()
+
+print(steps)
+wackycalcs()    
         
 
 
